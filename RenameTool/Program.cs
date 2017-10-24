f8d3c534cc3d3bcb8531f9e5e88aac0d8bcbf0e5
Liam <liam.wang@live.com>
2017-10-24 13:57:06 +0800

    Add project files.

---
+++ RenameTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RenameTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
